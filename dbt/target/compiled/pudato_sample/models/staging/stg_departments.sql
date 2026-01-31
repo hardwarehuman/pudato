69b8ec30fdc7f42c1aa39bd4df1e621ba60eebcc
hardwarehuman <github@klasser.net>
2026-01-31 11:49:29 -0800

    Add dbt artifacts to gitignore

--- dbt/target/compiled/pudato_sample/models/staging/stg_departments.sql
+++
@@ -1,9 +0,0 @@
--- Staging model for departments
--- Minimal transformation, mainly type casting and renaming
-
-select
-    department_id,
-    department_name,
-    budget_amount::decimal(15,2) as budget_amount,
-    fiscal_year::int as fiscal_year
-from "pudato"."main"."raw_departments"
